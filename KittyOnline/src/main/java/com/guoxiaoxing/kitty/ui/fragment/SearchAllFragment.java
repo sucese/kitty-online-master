d6d3d2c7439e8fb10bdf99c94f482d39bf0e357a
guoxiaoxing <allenwells@163.com>
2016-01-16 17:46:36 +0800

    update search ui

---
+++ KittyOnline/src/main/java/com/guoxiaoxing/kitty/ui/fragment/SearchAllFragment.java
@@ -0,0 +1,8 @@
+package com.guoxiaoxing.kitty.ui.fragment;
+
+import com.guoxiaoxing.kitty.bean.Base;
+
+/**
+ * Created by guoxiaoxing on 16-1-16.
+ */
+public class SearchAllFragment extends BaseF {
